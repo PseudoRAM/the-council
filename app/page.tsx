4dd61d9a8fe1c70036c9c06015a757b611f18559
Ben <PseudoRAM@users.noreply.github.com>
2025-02-22 16:32:49 +1100

    fix: build issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,10 @@
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import QuestionnaireForm from "@/components/context-gathering/Questionnaire";
 import { Hero } from "@/components/hero";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
 
 export default async function Home() {
   return (
     <>
       <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <QuestionnaireForm />
-      </main>
     </>
   );
 }
